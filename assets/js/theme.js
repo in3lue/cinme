e43a5b8d410532eb11bc3c09ab18dcebe309f7d1
Valeria Ferro <dg.vale.ferro@gmail.com>
2022-07-27 20:56:10 -0300

    fix:07-27-02

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -22,11 +22,11 @@
 		timeoutCountdown: 4000,
 		onLoadEvent: true,
 		browser: ['animation-duration', '-webkit-animation-duration', '-o-animation-duration'], // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser. The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-		
-		overlay : false,
-		overlayClass : 'animsition-overlay-slide',
-		overlayParentElement : 'html',
-		transition: function(url){ window.location.href = url; }
+
+		overlay: false,
+		overlayClass: 'animsition-overlay-slide',
+		overlayParentElement: 'html',
+		transition: function (url) { window.location.href = url; }
 	});
 
 
@@ -52,14 +52,14 @@
 	// Note: requires Easing plugin - http://gsgd.co.uk/sandbox/jquery/easing/
 	// =========================================================================
 
-	$('.sm-scroll').on("click",function() {
+	$('.sm-scroll').on("click", function () {
 		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
 				$('html,body').animate({
-				  scrollTop: target.offset().top
+					scrollTop: target.offset().top
 				}, 1000, 'easeInOutExpo');
 				return false;
 			}
@@ -73,25 +73,25 @@
 	// =========================================================================
 
 	// on hover
-	$('#header.header-hover').on("mouseenter",function() {
+	$('#header.header-hover').on("mouseenter", function () {
 		$('body').addClass('menu-open');
 	});
-	$('#header.header-hover').on("mouseleave",function() {
+	$('#header.header-hover').on("mouseleave", function () {
 		$('body').removeClass('menu-open');
 	});
 
 
 	// on click
-	$('#header.header-click .header-menu-icon, #header.header-fixed .header-menu-icon').on("click",function() {
+	$('#header.header-click .header-menu-icon, #header.header-fixed .header-menu-icon').on("click", function () {
 		$('body').addClass('menu-open');
 	});
-	$('.header-close').on("click",function() {
+	$('.header-close').on("click", function () {
 		$('body').removeClass('menu-open');
 	});
 
 	// close menu on page cover click
-	$('.page-cover').on("click",function() {
-	  $('body').removeClass('menu-open');
+	$('.page-cover').on("click", function () {
+		$('body').removeClass('menu-open');
 	});
 
 
@@ -105,16 +105,16 @@
 
 	// sub menu (on click)
 	// ====================
-	$('#menu .sub-menu-trigger').on('click', function() {
+	$('#menu .sub-menu-trigger').on('click', function () {
 		$(this).addClass('active');
 		$("#menu .sub-menu-trigger").not(this).removeClass('active');
 
 		$(this).parent().addClass('active').find('.sub-menu').slideToggle(300);
 		$("#menu .sub-menu-trigger").not(this).parent().removeClass('active').find('.sub-menu').slideUp(300);
-		
+
 		return false;
 	});
-	$('#header').on('mouseleave', function(){
+	$('#header').on('mouseleave', function () {
 		$('#menu .sub-menu-trigger').removeClass('active');
 		$('.sub-menu').delay(300).slideUp(300);
 	});
@@ -122,7 +122,7 @@
 
 	// mobile menu toggle
 	// ===================
-	$('.mobile-menu-trigger').on("click",function() {
+	$('.mobile-menu-trigger').on("click", function () {
 		$('body').toggleClass('menu-open');
 	});
 
@@ -136,11 +136,13 @@
 	// =======================================================================================
 
 	function init() {
-	var vidDefer = document.getElementsByTagName('iframe');
-	for (var i=0; i<vidDefer.length; i++) {
-	if(vidDefer[i].getAttribute('data-src')) {
-	vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
-	} } }
+		var vidDefer = document.getElementsByTagName('iframe');
+		for (var i = 0; i < vidDefer.length; i++) {
+			if (vidDefer[i].getAttribute('data-src')) {
+				vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
+			}
+		}
+	}
 	window.onload = init;
 
 
@@ -153,7 +155,7 @@
 
 	// init Isotope
 	var $container = $('.isotope-items-wrap');
-	$container.imagesLoaded(function() {
+	$container.imagesLoaded(function () {
 		$container.isotope({
 			itemSelector: '.isotope-item',
 			transitionDuration: '0.7s',
@@ -164,7 +166,7 @@
 	});
 
 	// Filter
-	$('.isotope-filter-links a').on("click",function(){
+	$('.isotope-filter-links a').on("click", function () {
 		var selector = $(this).attr('data-filter');
 		$container.isotope({
 			filter: selector
@@ -174,9 +176,9 @@
 
 	// Filter item active
 	var filterItemActive = $('.isotope-filter-links a');
-	filterItemActive.on('click', function(){
+	filterItemActive.on('click', function () {
 		var $this = $(this);
-		if ( !$this.hasClass('active')) {
+		if (!$this.hasClass('active')) {
 			filterItemActive.removeClass('active');
 			$this.addClass('active');
 		}
@@ -184,115 +186,40 @@
 
 
 	// Filter clickable/hover (clickable on small screens)
-	if ( $(window).width() < 992) {
+	if ($(window).width() < 992) {
 
 		// Filter clickable (effect on small screens)
-		$('.isotope-filter-button').on("click",function(){
+		$('.isotope-filter-button').on("click", function () {
 			$('.isotope-filter').toggleClass('iso-filter-open');
 		});
 
 		// Close filter if click on filter links (effect only on small screens)
-		$('ul.isotope-filter-links > li > a').on("click",function() {
+		$('ul.isotope-filter-links > li > a').on("click", function () {
 			$(".isotope-filter-button").click();
 		});
 
 	} else {
 
 		// Filter on hover
-		$('.isotope-filter').on("mouseenter",function(){
+		$('.isotope-filter').on("mouseenter", function () {
 			$('.isotope-filter').addClass('iso-filter-open');
 		});
-		$('.isotope-filter').on("mouseleave",function(){
+		$('.isotope-filter').on("mouseleave", function () {
 			$('.isotope-filter').removeClass('iso-filter-open');
 		});
 
 	}
-
-
-
-	// ===============================================
-	// OWL Carousel
-	// ===============================================
-
-	$(window).on('load', function() {
-
-		$('.owl-carousel').each(function(){
-			var $carousel = $(this);
-			$carousel.owlCarousel({
-
-				items: $carousel.data("items"),
-				// loop: $carousel.data("loop"),
-				margin: $carousel.data("margin"),
-				center: $carousel.data("center"),
-				startPosition: $carousel.data("start-position"),
-				// animateIn: $carousel.data("animate-in"),
-				// animateOut: $carousel.data("animate-out"),
-				// autoWidth: $carousel.data("autowidth"),
-				// autoHeight: $carousel.data("autoheight"),
-				// autoplay: $carousel.data("autoplay"),
-				// autoplayTimeout: $carousel.data("autoplay-timeout"),
-				// autoplayHoverPause: $carousel.data("autoplay-hover-pause"),
-				// autoplaySpeed: $carousel.data("autoplay-speed"),
-				// nav: $carousel.data("nav"),
-				// navText: ['', ''],
-				// navSpeed: $carousel.data("nav-speed"),
-				// dots: $carousel.data("dots"),
-				// dotsSpeed: $carousel.data("dots-speed"),
-				// mouseDrag: $carousel.data("mouse-drag"),
-				// touchDrag: $carousel.data("touch-drag"),
-				// dragEndSpeed: $carousel.data("drag-end-speed"),
-				// lazyLoad: $carousel.data("lazy-load"),
-				// video: true,
-				responsive: {
-					0: {
-						items: $carousel.data("mobile-portrait"),
-						center: false,
-					},
-					480: {
-						items: $carousel.data("mobile-landscape"),
-						center: false,
-					},
-					768: {
-						items: $carousel.data("tablet-portrait"),
-						center: false,
-					},
-					992: {
-						items: $carousel.data("tablet-landscape"),
-					},
-					1200: {
-						items: $carousel.data("items"),
-					}
-				}
-
-			});
-
-			// Mousewheel plugin
-			// var owl = $('.owl-mousewheel');
-			// owl.on('mousewheel', '.owl-stage', function (e) {
-			// 	if (e.deltaY > 0) {
-			// 		owl.trigger('prev.owl');
-			// 	} else {
-			// 		owl.trigger('next.owl');
-			// 	}
-			// 	e.preventDefault();
-			// });
-
-		});
-
-	});
-
-
 
 	// ==========================================
 	// Nicescroll
 	// Source: http://areaaperta.com/nicescroll/
 	// ==========================================
 
-	if ( $(window).width() > 768 ) { // Disable nicescroll on small screens.
-		 
+	if ($(window).width() > 768) { // Disable nicescroll on small screens.
+
 		// Init nicescroll (for horizontal scroll)
 		$(".hs-inner").niceScroll({
-			cursorcolor:"#4c4c4c", // change cursor color in hex
+			cursorcolor: "#4c4c4c", // change cursor color in hex
 			cursoropacitymin: 0, // change opacity when cursor is inactive (scrollabar "hidden" state), range from 1 to 0
 			cursoropacitymax: 1, // change opacity when cursor is active (scrollabar "visible" state), range from 1 to 0
 			cursorwidth: "4px", // cursor width in pixel (you can also write "5px")
@@ -376,20 +303,20 @@
 		loadYoutubeThumbnail: true, // You can automatically load thumbnails for youtube videos from youtube by setting loadYoutubeThumbnail true.
 		youtubeThumbSize: 'default', // You can specify the thumbnail size by setting respective number: 0, 1, 2, 3, 'hqdefault', 'mqdefault', 'default', 'sddefault', 'maxresdefault'.
 		youtubePlayerParams: { // Change youtube player parameters: https://developers.google.com/youtube/player_parameters
-		modestbranding: 0,
-		showinfo: 1,
-		controls: 1
+			modestbranding: 0,
+			showinfo: 1,
+			controls: 1
 		},
 
 		// Vimeo video options
 		loadVimeoThumbnail: true, // You can automatically load thumbnails for vimeo videos from vimeo by setting loadYoutubeThumbnail true.
 		vimeoThumbSize: 'thumbnail_medium', // Thumbnail size for vimeo videos: 'thumbnail_large' or 'thumbnail_medium' or 'thumbnail_small'.
 		vimeoPlayerParams: { // Change vimeo player parameters: https://developer.vimeo.com/player/embedding#universal-parameters 
-		byline : 1,
-		portrait : 1,
-		title: 1,
-		color : 'CCCCCC',
-		autopause: 1
+			byline: 1,
+			portrait: 1,
+			title: 1,
+			color: 'CCCCCC',
+			autopause: 1
 		}
 
 	});
@@ -410,7 +337,7 @@
 	// Source: http://www.webdesigncrowd.com/demo/circle-reveal-animation-12.23.13/
 	// ==============================================================================
 
-	$(".fav-count").on("click",function() {
+	$(".fav-count").on("click", function () {
 		var total = parseInt($(this).html(), 10);
 		if ($(this).parent().hasClass("active")) {
 			total -= 1;
@@ -421,7 +348,7 @@
 		$(this).parent().toggleClass("active");
 	});
 
-	$(".icon-heart").on("click",function() {
+	$(".icon-heart").on("click", function () {
 		var total = parseInt($(this).parent().siblings(".fav-count").first().html(), 10);
 		if ($(this).parent().parent().hasClass("active")) {
 			total -= 1;
@@ -440,17 +367,17 @@
 	// ===============================================
 
 	// E-mail Ajax Send
-	$("#contact-form").submit(function() { // Change (your contact form ID)
+	$("#contact-form").submit(function () { // Change (your contact form ID)
 		var th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "mail.php", // Change (mail.php path)
 			data: th.serialize()
-		}).done(function() {
+		}).done(function () {
 			alert("Thank you. Your message has been sent!");
-			setTimeout(function() {
-			// Done Functions
-			th.trigger("reset");
+			setTimeout(function () {
+				// Done Functions
+				th.trigger("reset");
 			}, 1000);
 		});
 		return false;
@@ -462,7 +389,7 @@
 	// Fade out element with page scroll
 	// ==================================
 
-	$(window).scroll(function(){
+	$(window).scroll(function () {
 		$(".fade-out-scroll-1").css("opacity", 1 - $(window).scrollTop() / 150);
 		$(".fade-out-scroll-2").css("opacity", 1 - $(window).scrollTop() / 250);
 		$(".fade-out-scroll-3").css("opacity", 1 - $(window).scrollTop() / 350);
@@ -479,17 +406,17 @@
 	// Element parallax effect
 	// ========================
 
-	$(window).scroll(function(){
+	$(window).scroll(function () {
 		var bgScroll = $(this).scrollTop();
-		if ( $(window).width() > 992) { // disable parallax on small devices.
+		if ($(window).width() > 992) { // disable parallax on small devices.
 
 			// parallax - transform
-			$('.parallax-1').css('transform', 'translate3d(0, '+ ((bgScroll * 0.1)) +'px, 0)');
-			$('.parallax-2').css('transform', 'translate3d(0, '+ ((bgScroll * 0.2)) +'px, 0)');
-			$('.parallax-3').css('transform', 'translate3d(0, '+ ((bgScroll * 0.3)) +'px, 0)');
-			$('.parallax-4').css('transform', 'translate3d(0, '+ ((bgScroll * 0.4)) +'px, 0)');
-			$('.parallax-5').css('transform', 'translate3d(0, '+ ((bgScroll * 0.5)) +'px, 0)');
-			$('.parallax-6').css('transform', 'translate3d(0, '+ ((bgScroll * 0.6)) +'px, 0)');
+			$('.parallax-1').css('transform', 'translate3d(0, ' + ((bgScroll * 0.1)) + 'px, 0)');
+			$('.parallax-2').css('transform', 'translate3d(0, ' + ((bgScroll * 0.2)) + 'px, 0)');
+			$('.parallax-3').css('transform', 'translate3d(0, ' + ((bgScroll * 0.3)) + 'px, 0)');
+			$('.parallax-4').css('transform', 'translate3d(0, ' + ((bgScroll * 0.4)) + 'px, 0)');
+			$('.parallax-5').css('transform', 'translate3d(0, ' + ((bgScroll * 0.5)) + 'px, 0)');
+			$('.parallax-6').css('transform', 'translate3d(0, ' + ((bgScroll * 0.6)) + 'px, 0)');
 
 			// parallax - top
 			// $('.parallax-top').css('top', ''+ ((bgScroll*0.9)) +'px');
@@ -505,9 +432,9 @@
 	// Gallery single top info toggle
 	// ===============================
 
-	$('.gsti-toggle-trigger').on("click",function() {
+	$('.gsti-toggle-trigger').on("click", function () {
 		$('.gsti-toggle').toggleClass('gsti-toggle-full');
-		$('.gsti-toggle-hidden').slideToggle( 400 );
+		$('.gsti-toggle-hidden').slideToggle(400);
 	});
 
 
@@ -517,7 +444,7 @@
 	// ======================
 
 	// Check to see if the window is top if not then display button
-	$(window).scroll(function(){
+	$(window).scroll(function () {
 		if ($(this).scrollTop() > 500) {
 			$('.scrolltotop').fadeIn();
 		} else {
